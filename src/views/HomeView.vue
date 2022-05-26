7b5385c06b20c10858773247fd4fff9b8558eea3
Suman Kim <kimsm9822@gmail.com>
2022-05-26 15:42:41 +0900

    minor change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <b-container class="bv-example-row mt-3 text-center">
-    <h3 class="underline-orange"><b-icon icon="house"></b-icon> HAPPY HOUSE</h3>
-    <br />
-    <img src="@/assets/main.png" width="1000px" height="600px"/>
-    <b-row>
-      <b-col></b-col>
-      <b-col cols="10">
-        <b-jumbotron
-          bg-variant="muted"
-          text-variant="dark"
-          border-variant="dark"
-        >
-          <template #header>SSAFY Home</template>
-
-          <template #lead>
-            슬기로운 싸피 생활 (:7기편) <br />
-            행운을 부르는 러~~~~키 Seven!!!!!
-          </template>
-
-          <hr class="my-4" />
-
-          <p>Vue + Bootstrap활용.</p>
-          <p>Bootstrap-vue는 버전 <b>4.6.1</b>을 권장합니다.</p>
-          <p><b>BoardList.vue</b>를 바꿔가면서 테스트하세요.</p>
-          <p>Bootstrap의 <b>table</b> 사용법을 익히게됩니다.</p>
-        </b-jumbotron>
-      </b-col>
-      <b-col></b-col>
-    </b-row>
-  </b-container>
-=======
   <div id="main">
     <div style="text-align: center">
       <h3 class="underline-orange mt-3 mb-5">
@@ -47,7 +15,6 @@
       - 매물의 위치를 지도에서 확인 가능&#10;
     </pre>
   </div>
->>>>>>> 810c7e21410b91107f1151169f5e2ca1a830cd40
 </template>
 
 <script>
